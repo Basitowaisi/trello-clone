40de444d7626b3ce75f7548689b339ce91d9dd04
Basit Bashir <basitowaisi@gmail.com>
2023-06-20 22:57:14 +0530

    Add ability to delete a task

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,4 +1,4 @@
-import { database } from "@/appwrite"
+import { database, storage } from "@/appwrite"
 import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn"
 import { create } from "zustand"
 
@@ -9,14 +9,30 @@ interface BoardState {
   updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void
   query: string
   setQuery: (query: string) => void
+  deleteTask: (taskIndex: number, todo: Todo, id: TypedColumn) => void
 }
 
-export const useBoardStore = create<BoardState>((set) => ({
+export const useBoardStore = create<BoardState>((set, get) => ({
   board: {
     columns: new Map<TypedColumn, Column>(),
   },
   query: "",
   setQuery: (query) => set({ query }),
+  deleteTask: async (taskIndex, todo, id) => {
+    const newColumns = new Map(get().board.columns)
+    newColumns.get(id)?.todos.splice(taskIndex, 1)
+    set({ board: { columns: newColumns } })
+
+    if (todo.image) {
+      await storage.deleteFile(todo.image.bucketId, todo.image.fileId)
+    }
+
+    await database.deleteDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id
+    )
+  },
   getBoard: async () => {
     const board = await getTodosGroupedByColumn()
     set({ board })
